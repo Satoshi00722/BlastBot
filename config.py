039ec48891fc4552cf60e7661379ba476cec26d6
Satoshi00722 <hort6536@gmail.com>
2026-01-25 16:06:21 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,11 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CRYPTOBOT_TOKEN = os.getenv("CRYPTOBOT_TOKEN")
-API_ID = int(os.getenv("API_ID"))
+
+API_ID = os.getenv("API_ID")
+API_ID = int(API_ID) if API_ID else None
+
 API_HASH = os.getenv("API_HASH")
+
+ADMIN_CHANNEL_ID = os.getenv("ADMIN_CHANNEL_ID")
+ADMIN_CHANNEL_ID = int(ADMIN_CHANNEL_ID) if ADMIN_CHANNEL_ID else None
